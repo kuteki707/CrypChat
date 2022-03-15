9c78d1b7de3b360d04709ea39af3deeec99cf5d1
Werner Banyacskay <werner.x64@gmail.com>
2022-03-15 12:03:55 +0200

    Removed the encrypt code

--- app/src/main/java/com/kuteki/crypchat/ChatActivity.kt
+++ app/src/main/java/com/kuteki/crypchat/ChatActivity.kt
@@ -130,115 +130,11 @@ class ChatActivity : AppCompatActivity() {
 }
 
 public fun encrypt(participant1:String, participant2:String, message:String):String {
-    var participant1_array = arrayListOf<Int>()
-    var participant2_array = arrayListOf<Int>()
-    var final_array = arrayListOf<Int>()
-    var encryptedMessage = ""
-
-    for (char in participant1) {
-        var n = char.toInt()
-        var s = 0
-
-        while (n > 0 || s >= 10) {
-            if (s >= 10) {
-                s = s % 10 + s / 10
-            } else {
-                s = s + n % 10
-                n = n / 10
-            }
-        }
-        participant1_array.add(s)
-    }
-
-    for (char in participant2) {
-        var n = char.toInt()
-        var s = 0
-
-        while (n > 0 || s >= 10) {
-            if (s >= 10) {
-                s = s % 10 + s / 10
-            } else {
-                s = s + n % 10
-                n = n / 10
-            }
-        }
-        participant2_array.add(s)
-    }
-
-    var i = 0
-
-    for (z in participant1_array) {
-        final_array.add(z + participant2_array[i])
-        i++
-    }
-
-    i=0
-
-    for(letter in message){
-        var letter_int = letter.toInt()
-        letter_int += final_array[i]
-        i++
-        encryptedMessage += letter_int.toChar()
-        if(i<20)
-            i=0
-    }
-
-    return encryptedMessage
+    //private encryption algorithm
+    return message
 }
 
 public fun decrypt(participant1: String, participant2: String, encryptedMessage:String):String{
-    var participant1_array = arrayListOf<Int>()
-    var participant2_array = arrayListOf<Int>()
-    var final_array = arrayListOf<Int>()
-    var decryptedMessage = ""
-
-    for(char in participant1) {
-        var n = char.toInt()
-        var s = 0
-
-        while(n > 0 || s >= 10){
-            if(s >= 10){
-                s = s % 10 + s / 10
-            }else {
-                s = s + n % 10
-                n = n / 10
-            }
-        }
-        participant1_array.add(s)
-    }
-
-    for(char in participant2) {
-        var n = char.toInt()
-        var s = 0
-
-        while(n > 0 || s >= 10){
-            if(s >= 10){
-                s = s % 10 + s / 10
-            }else {
-                s = s + n % 10
-                n = n / 10
-            }
-        }
-        participant2_array.add(s)
-    }
-
-    var i=0
-
-    for(z in participant1_array){
-        final_array.add(z + participant2_array[i])
-        i++
-    }
-
-    i=0
-
-    for(letter in encryptedMessage){
-        var letter_int = letter.toInt()
-        letter_int -= final_array[i]
-        i++
-        decryptedMessage += letter_int.toChar()
-        if(i<20)
-            i=0
-    }
-
-    return decryptedMessage
+    //private decryption algorithm
+    return encryptedMessage
 }
